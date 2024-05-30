cc631de755e6b0df410468d4cc702cb712c754d4
EmmaHenault <emmahenault31@gmail.com>
2024-05-30 09:58:37 +0200

    first

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,44 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.println(this.getName() + " takes off in the sky.");
+        }
+    }
+
+    @Override
+    public void ascend(int meters) {
+        if (this.flying) {
+            this.altitude += meters;
+            System.out.println(this.getName() + " flies upward, altitude: " + this.altitude + " metres.");
+        }
+    }
+
+    @Override
+    public void glide(){
+        System.out.print("glides into the air.");
+    }
+
+    @Override
+    public void descend(int meters){
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.println(this.getName() + " flies downward, altitude: " + this.altitude + " metres.");
+        }
+    }
+
+    @Override
+    public void land() {
+        if (this.flying && this.altitude <= 5) {
+            this.flying = false;
+            System.out.println(this.getName() + " atterrit sur le sol.");
+        } else {
+            System.out.println(this.getName() + " est trop haut, il ne peut pas atterrir.");
+        }
     }
 }
